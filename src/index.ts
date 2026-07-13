495507291626be4db4816ef4960b7edf067dbfde
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2026-07-12 21:25:25 -0300

    use basic logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 import filesController from './controllers/files.route'
 
-const app = new Hono()
+const app = new Hono().use(logger())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
