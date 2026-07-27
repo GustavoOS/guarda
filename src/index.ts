d46651efde093bfc5c3bc35b9fb774634861546e
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2026-07-26 21:45:19 -0300

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import filesController from './controllers/files.route'
 import webhooksController from './controllers/webhooks.route'
 
-const app = new Hono().use(logger())
+const app = new Hono().use(logger()).use(cors())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
